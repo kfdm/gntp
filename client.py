e9a16eac31329b639aee16dc5135a45b4f9cd9fa
Paul Traylor <paul@wegame.com>
2009-08-17 23:16:06 -0700

    Add a client growl script Add self.validate() GNTPRegister() and GNTPNotice() - Validates required headers Add self.format() to GNTPRegister() and GNTPNoticee() - Formats a Growl message to be sent

---
+++ client.py
@@ -0,0 +1,64 @@
+import gntp
+import socket
+import sys
+
+def send_growl(options,message=None):
+	#Send Registration
+	register = gntp.GNTPRegister()
+	register.add_header('Application-Name',options.app)
+	register.add_notification(options.name,True)
+	_send(options.host,options.port,register.format(),options.debug)
+	
+	#Send Notification
+	notice = gntp.GNTPNotice()
+	
+	#Required
+	notice.add_header('Application-Name',options.app)
+	notice.add_header('Notification-Name',options.name)
+	notice.add_header('Notification-Title',options.title)
+
+	#Optional
+	if options.sticky:
+		notice.add_header('Notification-Sticky',options.sticky)
+	if options.priority:
+		notice.add_header('Notification-Priority',options.priority)
+	
+	if message:
+		notice.add_header('Notification-Text',message)
+	
+	_send(options.host,options.port,notice.format(),options.debug)
+
+def _send(host,port,data,debug=False):
+	if debug: print '-----\n',data,'\n----'
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((host,port))
+	s.send(data)
+	response = s.recv(1024)
+	s.close()
+	
+	if debug: print '-----\n',response,'\n----'
+
+if __name__ == "__main__":
+	from optparse import OptionParser
+	parser = OptionParser(usage="usage: %prog [options] message")
+	
+	#Network
+	parser.add_option("-H","--hostname",dest="host",default="localhost")
+	parser.add_option("-p","--port",dest="port",help="port to listen on",type="int",default=23053)
+
+	#Required (Needs Defaults)
+	parser.add_option("-a","--appname",dest="app",default="pygntp")
+	parser.add_option("-n","--name",dest="name",default="Notification Name")
+	parser.add_option("-t","--title",dest="title",default="Notification Title")
+	
+	#Optional (Does not require Default)
+	parser.add_option("-d","--debug",dest='debug',help="Print raw growl packets",action="store_true",default=False)
+	parser.add_option("-s","--sticky",dest='sticky',help="Sticky Notification",action="store_true",default=None)
+	parser.add_option("--priority",dest="priority",help="-2 to 2  Default 0",type="int",default=None)
+	
+	(options, args) = parser.parse_args()
+	if len(args) > 0:
+		send_growl(options,' '.join(args))
+	else:
+		send_growl(options)
--- gntp.py
+++ gntp.py
@@ -12,15 +12,35 @@ class GNTPBase(object):
 			dict[key] = val
 			#print key,'\t\t\t',val
 		return dict
-		
+	def add_header(self,key,value):
+		self.headers[key] = value
 
 class GNTPRegister(GNTPBase):
 	def __init__(self,data=None):
 		self.headers	= {}
 		self.notifications = []
 		self.defaultNotifications = []
+		self.requiredHeaders = [
+			'Application-Name',
+			'Notifications-Count'
+		]
+		self.requiredNotification = [
+			'Notification-Name',
+		]
 		if data:
 			self.parse(data)
+		else:
+			self.headers['Application-Name'] = 'pygntp'
+			self.headers['Notifications-Count'] = 0
+	def validate(self):
+		for header in self.requiredHeaders:
+			print self.headers
+			if not self.headers.get(header,False):
+				raise Exception('Missing Registration Header: '+header)
+		for notice in self.notifications:
+			for header in self.requiredNotification:
+				if not notice.get(header,False):
+					raise Exception('Missing Notification Header: '+header)		
 	def parse(self,data):
 		self.raw = data
 		parts = self.raw.split('\r\n\r\n')
@@ -34,9 +54,33 @@ class GNTPRegister(GNTPBase):
 				self.notifications.append(notice['Notification-Name'])
 				if notice['Notification-Enabled'] == 'True':
 					self.defaultNotifications.append(notice['Notification-Name'])
-				
-	def __unicode__(self):
-		return self.headers
+	def add_notification(self,name,enabled=True):
+		notice = {}
+		notice['Notification-Name'] = name
+		notice['Notification-Enabled'] = str(enabled)
+			
+		self.notifications.append(notice)
+		self.headers['Notifications-Count'] = len(self.notifications)
+	def format(self):
+		self.validate()
+		SEP = u': '
+		EOL = u'\r\n'
+		
+		message = u'GNTP/1.0 REGISTER NONE ' + EOL
+		#Headers
+		for k,v in self.headers.iteritems():
+			message += k.encode('utf8') + SEP + str(v).encode('utf8') + EOL
+		
+		#Notifications
+		if len(self.notifications)>0:
+			for notice in self.notifications:
+				message += EOL
+				for k,v in notice.iteritems():
+					message += k.encode('utf8') + SEP + str(v).encode('utf8') + EOL
+		
+		message += EOL
+		return message
+	
 	def send(self):
 		print
 		print '=Registration Object='
@@ -49,17 +93,33 @@ class GNTPRegister(GNTPBase):
 		print
 
 class GNTPNotice(GNTPBase):
-	def __init__(self,data=None):
+	def __init__(self,data=None,app=None,name=None):
 		self.raw	= data
 		self.headers	= {}
 		self.resources	= {}
+		self.requiredHeaders = [
+			'Application-Name',
+			'Notification-Name',
+			'Notification-Title'
+		]
 		if data:
 			self.parse(data)
+		else:
+			if app:
+				self.headers['Application-Name'] = app
+			if name:
+				self.headers['Notification-Name'] = name
+	def validate(self):
+		for header in self.requiredHeaders:
+			print self.headers
+			if not self.headers.get(header,False):
+				raise Exception('Missing Notification Header: '+header)
 	def parse(self,data):
 		self.raw = data
 		parts = self.raw.split('\r\n\r\n')
 		self.headers = self.parse_dict(parts[0])
 		if len(parts) > 1:
+			print 'Extra parts'
 			print parts[1:]
 			if parts[1] == '': return
 			item					= self.parse_dict(parts[1])
@@ -76,9 +136,17 @@ class GNTPNotice(GNTPBase):
 		print '==Resources=='
 		print self.resources
 		print
-			
-	def __unicode__(self):
-		return self.headers
+	def format(self):
+		SEP = u': '
+		EOL = u'\r\n'
+		
+		message = u'GNTP/1.0 NOTIFY NONE ' + EOL
+		#Headers
+		for k,v in self.headers.iteritems():
+			message += k.encode('utf8') + SEP + str(v).encode('utf8') + EOL
+		
+		message += EOL
+		return message
 
 class GNTPResponse(object):
 	def format(self):
