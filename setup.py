33847c46b2583cfd7ce59b8fa1150f3875db7c64
Paul Traylor <paul@wegame.com>
2009-08-28 19:51:38 -0700

    Adding a simple setup.py script Renaming README so that it should only shows up in github

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name='gntp',
+	description='Growl Notification Transport Protocol for Python',
+	author='Paul Traylor',
+	url='http://github.com/kfdm/gntp/',
+	version='0.1',
+	py_modules=['gntp'],
+	)
