fc597ea836e011f948b9a4a1394f6cac64658b93
Paul Traylor <paul@wegame.com>
2010-05-18 19:40:45 -0700

    Rewrite the client script to match the Mac Growl Python library better

--- client.py
+++ client.py
@@ -1,52 +1,110 @@
+#!/usr/bin/env python
+"""
+A Python module that uses GNTP to post messages
+Mostly mirrors the Growl.py file that comes with Mac Growl
+http://code.google.com/p/growl/source/browse/Bindings/python/Growl.py
+"""
 import gntp
 import socket
-import sys
 
-def send_growl(options,message=None):
-	#Send Registration
-	register = gntp.GNTPRegister()
-	register.add_header('Application-Name',options.app)
-	register.add_notification(options.name,True)
+class GrowlNotifier(object):
+	applicationName = 'Python GNTP'
+	notifications = []
+	defaultNotifications = []
+	applicationIcon = None
 	
-	if options.password:
-		register.set_password(options.password)
+	#GNTP Specific
+	debug = False
+	password = None
+	hostname = None
+	port = 23053
 	
-	_send(options.host,options.port,register.encode(),options.debug)
-	
-	#Send Notification
-	notice = gntp.GNTPNotice()
-	
-	#Required
-	notice.add_header('Application-Name',options.app)
-	notice.add_header('Notification-Name',options.name)
-	notice.add_header('Notification-Title',options.title)
-	
-	if options.password:
-		notice.set_password(options.password)
+	def __init__(self, applicationName=None, notifications=None, defaultNotifications=None, applicationIcon=None, hostname=None, password=None, port=None, debug=False):
+		if applicationName:
+			self.applicationName = applicationName
+		assert self.applicationName, 'An application name is required.'
 
-	#Optional
-	if options.sticky:
-		notice.add_header('Notification-Sticky',options.sticky)
-	if options.priority:
-		notice.add_header('Notification-Priority',options.priority)
-	if options.icon:
-		notice.add_header('Notification-Icon',options.icon)
-	
-	if message:
-		notice.add_header('Notification-Text',message)
-	
-	_send(options.host,options.port,notice.encode(),options.debug)
+		if notifications:
+			self.notifications = list(notifications)
+		assert self.notifications, 'A sequence of one or more notification names is required.'
 
-def _send(host,port,data,debug=False):
-	if debug: print '<Sending>\n',data,'\n</Sending>'
+		if defaultNotifications is not None:
+			self.defaultNotifications = list(defaultNotifications)
+		elif not self.defaultNotifications:
+			self.defaultNotifications = list(self.notifications)
+
+		if applicationIcon is not None:
+			self.applicationIcon = self._checkIcon(applicationIcon)
+		elif self.applicationIcon is not None:
+			self.applicationIcon = self._checkIcon(self.applicationIcon)
+		
+		#GNTP Specific
+		if password:
+			self.password = password
+		
+		if hostname:
+			self.hostname = hostname
+		assert self.hostname, 'Requires valid hostname'
+		
+		if port:
+			self.port = int(port)
+		assert isinstance(self.port,int), 'Requires valid port'
+		
+		if debug:
+			self.debug = debug
+		
+	def _checkIcon(self, data):
+		'''
+		Check the icon to see if it's valid
+		@param data: 
+		@todo Consider checking for a valid URL
+		'''
+		return data
 	
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((host,port))
-	s.send(data)
-	response = gntp.parse_gntp(s.recv(1024))
-	s.close()
+	def register(self):
+		'''
+		Send GNTP Registration
+		'''
+		register = gntp.GNTPRegister()
+		register.add_header('Application-Name',self.applicationName)
+		for notification in self.notifications:
+			enabled = notification in self.defaultNotifications
+			register.add_notification(options.name,enabled)
+		if self.password:
+			register.set_password(self.password)
+		self.send(register.encode())
 	
-	if debug: print '<Recieved>\n',response,'\n</Recieved>'
+	def notify(self, noteType, title, description, icon=None, sticky=False, priority=None):
+		'''
+		Send a GNTP notifications
+		'''
+		assert noteType in self.notifications
+		notice = gntp.GNTPNotice()
+		notice.add_header('Application-Name',self.applicationName)
+		notice.add_header('Notification-Name',noteType)
+		notice.add_header('Notification-Title',title)
+		if self.password:
+			notice.set_password(self.password)
+		if sticky:
+			notice.add_header('Notification-Sticky',sticky)
+		if priority:
+			notice.add_header('Notification-Priority',priority)
+		if icon:
+			notice.add_header('Notification-Icon',self._checkIcon(icon))
+		if message:
+			notice.add_header('Notification-Text',message)
+		self.send(notice.encode())
+	def send(self,data):
+		'''
+		Send the GNTP Packet
+		'''
+		if self.debug: print '<Sending>\n',data,'\n</Sending>'
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((self.hostname,self.port))
+		s.send(data)
+		response = gntp.parse_gntp(s.recv(1024))
+		s.close()
+		if self.debug: print '<Recieved>\n',response,'\n</Recieved>'
 
 if __name__ == "__main__":
 	from optparse import OptionParser
@@ -55,7 +113,7 @@ if __name__ == "__main__":
 	#Network
 	parser.add_option("-H","--hostname",dest="host",default="localhost")
 	parser.add_option("-p","--port",dest="port",help="port to listen on",type="int",default=23053)
-	parser.add_option("-P","--password",dest="password",help="network password")
+	parser.add_option("-P","--password",dest="password",default=None,help="network password")
 
 	#Required (Needs Defaults)
 	parser.add_option("-a","--appname",dest="app",default="pygntp")
@@ -69,7 +127,24 @@ if __name__ == "__main__":
 	parser.add_option("-i","--icon",dest="icon",help="Icon for notification (Only supports URL currently)",default=False)
 	
 	(options, args) = parser.parse_args()
-	if len(args) > 0:
-		send_growl(options,' '.join(args))
-	else:
-		send_growl(options)
+	if len(args) > 0: message = ' '.join(args)
+	else: message = ''
+	
+	growl = GrowlNotifier(
+		applicationName = options.app,
+		notifications = [options.name],
+		defaultNotifications = [options.name],
+		hostname = options.host,
+		password = options.password,
+		port = options.port,
+		debug = options.debug,
+	)
+	growl.register()
+	growl.notify(
+		noteType = options.name,
+		title = options.title,
+		description = message,
+		icon = options.icon,
+		sticky = options.sticky,
+		priority = options.priority,
+	)
