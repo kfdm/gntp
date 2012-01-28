e0d686444d7baf58ba31af775286cd199ef70861
Paul Traylor <kungfudiscomonkey@gmail.com>
2012-01-28 15:17:14 -0800

    Fabfile updates

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,21 @@
 from fabric.api import local
 
+
 def clean():
 	"Remove all generated files"
 	local("rm -rf build dist")
+	local("find . -name *.pyc -delete")
+
 
 def test():
 	"Run unittests"
 	local('py.test')
 
+
 def html():
 	"Generate Sphinx documentation"
-	local("make html")
+	local("sphinx-build -b html -d build/doctrees docs build/html")
+
+
+def build():
+	local("python setup.py build")
