4f6b61e626454cfc9018c273c01ab1a31d888abb
Paul Traylor <kungfudiscomonkey@gmail.com>
2012-10-21 20:44:58 -0700

    Initial socket error test

---
+++ gntp/test/test_errors.py
@@ -0,0 +1,21 @@
+"""
+Test the various error condtions
+
+This test runs with the gntp.config module so that we can
+get away without having to hardcode our password in a test
+script. Please fill out your ~/.gntp config before running
+"""
+
+from gntp.test import GNTPTestCase
+from gntp import BaseError
+
+
+class TestErrors(GNTPTestCase):
+	def test_connection_error(self):
+		#self.growl.hostname = '0.0.0.0'
+		# Port 9 would be the  discard protocol. We just want a "null port"
+		# for testing
+		# http://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+		self.growl.port = 9
+		with self.assertRaises(BaseError):
+			self._notify(description='Connection Error')
