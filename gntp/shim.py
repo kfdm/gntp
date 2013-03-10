c124ada25d926a7ff6b9cb2976785d1032830456
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-03-10 15:15:29 -0700

    Add StringIO to our shim

--- gntp/shim.py
+++ gntp/shim.py
@@ -2,3 +2,8 @@ try:
 	from ConfigParser import RawConfigParser
 except ImportError:
 	from configparser import RawConfigParser
+
+try:
+	from StringIO import StringIO
+except ImportError:
+	from io import StringIO
