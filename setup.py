e1f2a12ca4514af586b4fa21d3390000818c3f3d
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-03-10 18:16:25 -0700

    Add Python 3.3 to setup.py

--- setup.py
+++ setup.py
@@ -22,9 +22,12 @@ setup(
 		'License :: OSI Approved :: MIT License',
 		'Natural Language :: English',
 		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
 		'Programming Language :: Python :: 2.5',
 		'Programming Language :: Python :: 2.6',
 		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
 	],
 	entry_points={
 		'console_scripts': [
