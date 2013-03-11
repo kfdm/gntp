10dba9826473fd83ec803d8f6428e38afecca0a9
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-03-10 18:17:23 -0700

    Add PyPy to the setup.py list since we test it through tox

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
 		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3',
 		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: Implementation :: PyPy',
 	],
 	entry_points={
 		'console_scripts': [
