1db46b8c04ad8154ca2423612120623dd9cfb413
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-03-10 18:19:41 -0700

    Set the Development Status to Beta

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 	packages=['gntp'],
 	# http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	classifiers=[
-		'Development Status :: 3 - Alpha',
+		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: MIT License',
 		'Natural Language :: English',
